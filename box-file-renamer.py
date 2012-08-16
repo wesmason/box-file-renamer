c71a179692ccdd81859f8a496fe7f27b35bf1855
Wes Mason <wmason@fiksu.com>
2012-08-16 16:17:40 -0400

    Added parser and usage information.
    - Added a parser to process command line arguments.
    - Added usage information by adding the parser.

--- box-file-renamer.py
+++ box-file-renamer.py
@@ -7,15 +7,20 @@ import time
 import argparse
 import re
 
-folderChangeCount = 0
-fileChangeCount = 0
-errorCount = 0
-unsafeCharacters = '[\\":<>|*?!]'
-fileList = []
-subFolderList = []
-rootDirectory = sys.argv[1]
 ###########################################################################
-#Logging Setup
+# Global variables
+###########################################################################
+
+errorCount = 0						# Number of errors for exit code and information.
+fileChangeCount = 0					# Track the number of files that are renamed.
+fileList = []						# The list of files
+folderChangeCount = 0				# Track the number of folders that are renamed.
+rootDirectory = ""			# The root directory whence the program recurses.
+subFolderList = []					# The list of subfolders of the root directory.
+unsafeCharacters = '[\\":<>|*?!]'	# The characters that are 'unsafe' and will be replaced with hyphens.
+
+###########################################################################
+# Logging Setup
 ###########################################################################
 
 #If the logging folder doesn't exist, create it:
@@ -29,7 +34,7 @@ if not os.path.exists("./logs"):
 logger = logging.getLogger('box-file-renamer')
 logger.setLevel(logging.DEBUG)
 
-#Create console handler and set level to debug;
+#Create console handler and set level to debug
 ch = logging.FileHandler('logs/box-file-renamer_' + time.strftime("%Y-%m-%d_%H-%M-%S" + '.log', time.localtime()),
 	mode='a',
 	encoding=None,
@@ -47,6 +52,15 @@ logger.addHandler(ch)
 # Helper Methods
 ###########################################################################
 
+# Returns True if the file or folder has a safe name, and False if path has an unsafe name
+def isSafe(path):
+	#These are the unsafe characters, as explained by the Box Sync FAQ here:
+	#https://support.box.com/entries/20364878-problems-syncing
+	if re.search(unsafeCharacters, path):
+		logger.debug('Unsafe characters detected: ' + path)
+		return False
+	return True
+
 # Recursively walks the path at the given 'path' argument and returns a list of all files located therein
 def listOfFiles(path):
 	toReturn = []
@@ -56,38 +70,12 @@ def listOfFiles(path):
 	return toReturn
 
 # Recursively walks the path at the given 'path' argument and returns a list of all folders located therein
-def listOfSubFolders(path):
+def listOfFolders(path):
 	toReturn = []
 	for root, subFolders, files in os.walk(path):
 		for subFolder in subFolders:
 			toReturn.append(os.path.join(root,subFolder))
 	return toReturn
-
-# Safely renames all the folders and subfolders of the given path, recursively.
-def renameFolders(path): 
-	for current in listOfSubFolders(path):
-		if not isSafe(current):
-			safeRename(current, 0)
-			global folderChangeCount
-			folderChangeCount += 1
-
-# Safely renames all the files located at the given path, recursively.
-def renameFiles(path): 
-	for current in listOfFiles(path):
-		if not isSafe(current):
-			logger.debug('Renaming: "%(original)s"' % {"original" : current})
-			safeRename(current, 0)
-			global fileChangeCount
-			fileChangeCount += 1
-
-# Returns True if the file or folder has a safe name, and False if path has an unsafe name
-def isSafe(path):
-	#These are the unsafe characters, as explained by the Box Sync FAQ here:
-	#https://support.box.com/entries/20364878-problems-syncing
-	if re.search(unsafeCharacters, path):
-		logger.debug('Unsafe characters detected: ' + path)
-		return False
-	return True
 
 # Actually renames the given file or folder to the specified new name.
 def rename(toRename, newName):
@@ -100,6 +88,23 @@ def rename(toRename, newName):
 			{"errorNumber" : e.errno,
 			"errorText" : e.strerror}
 			)
+
+# Safely renames all the files located at the given path, recursively.
+def renameFiles(path): 
+	for current in listOfFiles(path):
+		if not isSafe(current):
+			logger.debug('Renaming: "%(original)s"' % {"original" : current})
+			safeRename(current, 0)
+			global fileChangeCount
+			fileChangeCount += 1
+
+# Safely renames all the folders and subfolders of the given path, recursively.
+def renameFolders(path): 
+	for current in listOfFolders(path):
+		if not isSafe(current):
+			safeRename(current, 0)
+			global folderChangeCount
+			folderChangeCount += 1
 
 # Performs check to safely rename a file or folder:
 # - Unique Name: Checks to make sure that the new name isn't already in use by another file.
@@ -131,25 +136,42 @@ def safeRename(toRename, attemptNumber):
 # Main Script
 ###########################################################################
 
-logger.info("The script is starting.")
+def main():
 
-# First, we'll rename the folders.
-renameFolders(rootDirectory)
+	logger.info("The script is starting.")
 
-# Now, we'll rename files.
-renameFiles(rootDirectory)
+	# Creates a parser for our arguments and options
+	parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+		description='''Replaces unsafe characters in file and folder names before uploading files to Box.''',
+		epilog='''For more information, please visit: https://github.com/wesmason/box-file-renamer\n(c) 2012 Wes Mason and Fiksu, Inc.''')
 
-if errorCount > 0:
-	logger.info("The script completed with %(numberOfErrors)s errors." % {"numberOfErrors" : errorCount})
-	print "%(numberOfErrors)s errors occured. Please check the logs." % {"numberOfErrors" : errorCount}
-	sys.exit(1)
-else:
-	logger.info("The script completed with no errors. %(fileChangeCount)s file(s) changed. %(folderChangeCount)s folder(s) changed." %
+	# Adds arguments to our parser.
+	#parser.add_argument('--loglevel', metavar="level", type=int, nargs=1, help="How verbose to be with logging. The default is none.")
+	parser.add_argument('rootDirectory', metavar="rootDirectory", type=str, nargs=1, help="The directory through which the script should recurse.")
+	
+	# Prases the arguments
+	args = parser.parse_args()
+
+	# First, we'll rename the folders.
+	renameFolders(args.rootDirectory[0])
+
+	# Now, we'll rename files.
+	renameFiles(args.rootDirectory[0])
+
+	if errorCount > 0:
+		logger.info("The script completed with %(numberOfErrors)s errors." % {"numberOfErrors" : errorCount})
+		print "%(numberOfErrors)s errors occured. Please check the logs." % {"numberOfErrors" : errorCount}
+		sys.exit(1)
+	else:
+		logger.info("The script completed with no errors. %(fileChangeCount)s file(s) changed. %(folderChangeCount)s folder(s) changed." %
 		{"fileChangeCount" : fileChangeCount,
 		"folderChangeCount" : folderChangeCount}
 		)
-	print("The script completed with no errors. %(fileChangeCount)s file(s) changed. %(folderChangeCount)s folder(s) changed." %
-		{"fileChangeCount" : fileChangeCount,
-		"folderChangeCount" : folderChangeCount}
-		)
-	sys.exit(0)
+		print("The script completed with no errors. %(fileChangeCount)s file(s) changed. %(folderChangeCount)s folder(s) changed." %
+			{"fileChangeCount" : fileChangeCount,
+			"folderChangeCount" : folderChangeCount}
+			)
+		sys.exit(0)
+	
+if __name__ == '__main__':
+	main()
