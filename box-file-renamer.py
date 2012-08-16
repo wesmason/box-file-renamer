66e842f259668754ea4ce8f4d801636451f82e85
Wes Mason <wmason@fiksu.com>
2012-08-16 11:33:57 -0400

    Added a check: - Unique Path Name: Ensures that a file or folder name is not already used when a file or folder is renamed. If it is, then the script will add ".CONFLICT-X" as a suffix, where 'X' is incremented until there is no longer a name conflict.

--- box-file-renamer.py
+++ box-file-renamer.py
@@ -7,7 +7,10 @@ import time
 import argparse
 import re
 
-unsafeCharacters = '[\\":<>|*?]'
+folderChangeCount = 0
+fileChangeCount = 0
+errorCount = 0
+unsafeCharacters = '[\\":<>|*?!]'
 fileList = []
 subFolderList = []
 rootDirectory = sys.argv[1]
@@ -17,7 +20,10 @@ rootDirectory = sys.argv[1]
 
 #If the logging folder doesn't exist, create it:
 if not os.path.exists("./logs"):
-	os.mkdir("./logs")
+	try:
+		os.mkdir("./logs")
+	except OSError:
+		print "There was a problem creating the logging directory. Make sure that you have permission to write to the folder whence this script is executed."
 
 #Create the logger
 logger = logging.getLogger('box-file-renamer')
@@ -38,10 +44,10 @@ ch.setFormatter(formatter)
 logger.addHandler(ch)
 
 ###########################################################################
-# Main Script
+# Helper Methods
 ###########################################################################
 
-# Returns the current list of files in the folder, recursively.
+# Recursively walks the path at the given 'path' argument and returns a list of all files located therein
 def listOfFiles(path):
 	toReturn = []
 	for root, subFolders, files in os.walk(path):
@@ -49,7 +55,7 @@ def listOfFiles(path):
 			toReturn.append(os.path.join(root,file))
 	return toReturn
 
-# Generate a list of all the files in the root directory, recursively.
+# Recursively walks the path at the given 'path' argument and returns a list of all folders located therein
 def listOfSubFolders(path):
 	toReturn = []
 	for root, subFolders, files in os.walk(path):
@@ -61,14 +67,20 @@ def listOfSubFolders(path):
 def renameFolders(path): 
 	for current in listOfSubFolders(path):
 		if not isSafe(current):
-			safeRename(current)
+			safeRename(current, 0)
+			global folderChangeCount
+			folderChangeCount += 1
+
 # Safely renames all the files located at the given path, recursively.
 def renameFiles(path): 
 	for current in listOfFiles(path):
 		if not isSafe(current):
-			safeRename(current)
+			logger.debug('Renaming: "%(original)s"' % {"original" : current})
+			safeRename(current, 0)
+			global fileChangeCount
+			fileChangeCount += 1
 
-# Returns True if the file or folder has a safe name.
+# Returns True if the file or folder has a safe name, and False if path has an unsafe name
 def isSafe(path):
 	#These are the unsafe characters, as explained by the Box Sync FAQ here:
 	#https://support.box.com/entries/20364878-problems-syncing
@@ -77,14 +89,64 @@ def isSafe(path):
 		return False
 	return True
 
-# Safely renames the file or folder at the given path.
-def safeRename(toRename):
-	logger.debug('Renaming: "%(original)s"' % {"original" : toRename}) 
-	renamed = re.sub(unsafeCharacters,"-", toRename)
-	logger.debug('Renamed: "%(original)s" => "%(renamed)s"' % {"original" : toRename, "renamed" : renamed}) 
+# Actually renames the given file or folder to the specified new name.
+def rename(toRename, newName):
+	# Attempt to actually rename the file now.
+	try:
+		os.rename(toRename, newName)
+	except OSError as e:
+		errorCount += 1
+		logger.error("OS Error: An attempt to rename %(toRename)s failed. (Error %(errorNumber)s: %(errorText)s)" %
+			{"errorNumber" : e.errno,
+			"errorText" : e.strerror}
+			)
 
-# First, we need to rename folders.
+# Performs check to safely rename a file or folder:
+# - Unique Name: Checks to make sure that the new name isn't already in use by another file.
+#	This could happen during mass renaming (e.g., 1:1.txt => 1-1.txt; 1|1.txt => 1-1.txt) and result in loss of data.
+# 
+# To accomplish this, the method calls recurses and tries adding the suffix ".CONFLICT-X", where X is the number of
+# conflicts that existed because of identical file names.
+def safeRename(toRename, attemptNumber):
+	
+	# Substitutes any unsafe characters with hyphens, a safe character for path names in Windows, Linux, and OS X.
+	newName = re.sub(unsafeCharacters,"-", toRename)
+
+	# If the attempt number is greater than zero, we'll need to start adding a conflict suffix.
+	if attemptNumber > 0:
+		newName = "%(newName)s.CONFLICT-%(attemptNumber)s" % {"newName" : newName, "attemptNumber" : attemptNumber}	
+	# If the newName doesn't exist, we have a path name we can use.
+	if not os.path.exists(newName):
+		rename(toRename, newName)
+		logger.debug('Renamed: "%(original)s" => "%(newName)s"' % {"original" : toRename, "newName" : newName}) 
+	# If the newName does exist, we'll need to try again.
+	else:
+		attemptNumber = attemptNumber + 1
+		safeRename(toRename, attemptNumber)
+
+###########################################################################
+# Main Script
+###########################################################################
+
+logger.info("The script is starting.")
+
+# First, we'll rename the folders.
 renameFolders(rootDirectory)
 
-# Now, we can rename files.
+# Now, we'll rename files.
 renameFiles(rootDirectory)
+
+if errorCount > 0:
+	logger.info("The script completed with %(numberOfErrors)s errors." % {"numberOfErrors" : errorCount})
+	print "%(numberOfErrors)s errors occured. Please check the logs." % {"numberOfErrors" : errorCount}
+	sys.exit(1)
+else:
+	logger.info("The script completed with no errors. %(fileChangeCount)s file(s) changed. %(folderChangeCount)s folder(s) changed." %
+		{"fileChangeCount" : fileChangeCount,
+		"folderChangeCount" : folderChangeCount}
+		)
+	print("The script completed with no errors. %(fileChangeCount)s file(s) changed. %(folderChangeCount)s folder(s) changed." %
+		{"fileChangeCount" : fileChangeCount,
+		"folderChangeCount" : folderChangeCount}
+		)
+	sys.exit(0)
